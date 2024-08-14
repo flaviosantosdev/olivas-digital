6bbf8da273cf62d80e9c795fe9371028833ed20d
flaviosantosdev <flavio-santos92@hotmail.com>
2024-08-14 18:23:40 -0300

    feat:add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import "../styles/globals.css";
-
-
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -20,11 +19,29 @@ function MyApp({ Component, pageProps }) {
           sizes="32x32"
           href="/favicon/favicon-32x32.png"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
-        
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
       </Head>
+      <Script
+        id="HotJarAnalytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5093513,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+        }}
+      ></Script>
       <Component {...pageProps} />
     </>
   );
